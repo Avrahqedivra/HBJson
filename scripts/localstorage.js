00b491e127500c7dee684518979151c8f9363ce9
Jean-Michel Cohen <jmc_96@hotmail.com>
2022-09-29 12:39:39 +0200

    added website name to cookie naming

--- scripts/localstorage.js
+++ scripts/localstorage.js
@@ -1,4 +1,4 @@
-cookieSettingsName = "hbjson_settings";
+cookieSettingsName = window.location.hostname+"_hbjson_settings";
 settingsValidity = 5;	    // 5 days
 settings = [ { config: { "theme": "theme-dark", hidetg: false } }, { "map": { "zoom" : 6.5 } }, { name: "openbridges", "open": true }, {"name": "masters", "open": true }, { "name": "peers", "open": true }];
 
